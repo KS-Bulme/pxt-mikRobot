20381ccd63228601c61068fda2823f1878bd7823
KS-Bulme <50808568+KS-Bulme@users.noreply.github.com>
2019-10-25 09:50:47 +0200

    Update mikRobot.ts

--- mikRobot.ts
+++ mikRobot.ts
@@ -147,16 +147,17 @@ namespace mikRobot {
         if (!initialized) {
             initPCA9685()
         }
-	if (pos > 90) {
-            pos = MAX_PULSE_WIDTH
+	if (pos > 180) {
+            pos = 180
         }
-        if (pos < 90) {
-            pos = MIN_PULSE_WIDTH
+        if (pos < 0) {
+            pos = 0
         }
 	    
 	// map 180 to 4096 (http://wiki.sunfounder.cc/index.php?title=PCA9685_16_Channel_12_Bit_PWM_Servo_Driver)
 	// pos = MIN_PULSE_WIDTH + pos * (MAX_PULSE_WIDTH-MIN_PULSE_WIDTH)/180.0;
 	// pos = DEFAULT_PULSE_WIDTH;
+	pos = (MIN_PULSE_WIDTH + pos * (MAX_PULSE_WIDTH-MIN_PULSE_WIDTH)/180.0)/ 1000000 * FREQUENCY * 4096;
         
         if (index == 1) {
             setPwm(8, 0, pos)
