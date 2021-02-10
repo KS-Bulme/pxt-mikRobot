de73ed3de5aea4c24323438e436b4739cbc6892a
KS-Bulme <50808568+KS-Bulme@users.noreply.github.com>
2021-02-10 20:57:10 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,18 @@
-// tests go here; this will not be compiled when this package is used as a library
+input.onButtonPressed(Button.A, function () {
+    mikRobot.Servo(Servos.S1, 0)  // PWM test A
+    for (let index = 0; index < 4; index++) {
+        mikRobot.RunDelay(Dir.forward, 50, 2)  // motor test
+        mikRobot.RunDelay(Dir.turnLeft, 50, 0.5)  // turn left
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    mikRobot.Servo(Servos.S1, 180)  // PWM test B
+})
+mikRobot.SensorCalibrated()  // calibrate line sensors
+basic.forever(function () {
+    led.plotBarGraph(
+    mikRobot.Ultrasonic(),  // ultrasonic sensor test
+    40
+    )
+    basic.pause(100)
+})
